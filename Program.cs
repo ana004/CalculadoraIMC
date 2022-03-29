dd942e79975b7fefd953bbdcc6a09cb1f1d057be
ana004 <paulaana08002019@gmail.com>
2022-03-29 15:12:13 -0300

    Adicionada a funcionalidade de saber a faixa do IMC

--- Program.cs
+++ Program.cs
@@ -16,9 +16,24 @@ namespace EstudoDotNet
             Console.WriteLine("Qual é o seu peso em Kg? ");
             float peso = Convert.ToSingle(Console.ReadLine());
 
-            double imc = peso / (altura*altura);
+            double imc = Math.Round(peso / (altura*altura), 2);
 
-            Console.WriteLine("Nome: " + nome + "Peso: " + peso + "Altura: " + altura +  "IMC: " + imc);
+            Console.WriteLine("Nome: " + nome + " " + "Peso: " + peso + " " + "Altura: " + altura + " " +  "IMC: " + imc);
+
+            if (imc < 17)
+                Console.WriteLine("Muito abaixo do peso");
+            else if (imc >= 17 && imc <= 18.49)
+                Console.WriteLine("Abaixo do peso");
+            else if (imc >= 18.50 && imc <= 24.99)
+                Console.WriteLine("Peso normal");
+            else if (imc >= 25 && imc <= 29.99)
+                Console.WriteLine("Acima do peso");
+            else if (imc >= 30 && imc <= 34.99)
+                Console.WriteLine("Obesidade I");
+            else if (imc >= 35 && imc <= 39.99)
+                Console.WriteLine("Obesidade II (severa)");
+            else if (imc >= 40)
+                Console.WriteLine("Obesidade III (mórbida)");
         }
     }
 }
