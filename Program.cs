686c54b10ebaac72c6088f5d4c6e0032a78eb773
ana004 <paulaana08002019@gmail.com>
2022-03-29 14:32:05 -0300

    Calculadora IMC que recebe dados e apresenta o resultado calculado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace EstudoDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Qual é a sua altura em metros? ");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Qual é o seu peso em Kg? ");
+            float peso = Convert.ToSingle(Console.ReadLine());
+
+            double imc = peso / (altura*altura);
+
+            Console.WriteLine("Nome: " + nome + "Peso: " + peso + "Altura: " + altura +  "IMC: " + imc);
+        }
+    }
+}
